12e7fed09781fff0af1402793976f6ada9717094
Gürkan Uzunca <selam@gurkanuzunca.com>
2016-03-26 14:33:39 +0200

    Controller

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -7,7 +7,6 @@ class HomeController extends Controller
 
     public function indexAction($request, $response, $args)
     {
-        print_r($args);
         return $this->render('index.php', array('a' => 'sdasds'));
     }
 
