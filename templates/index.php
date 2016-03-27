49a4ac88f927942b86af43941c3fe3e1c64bbab3
Gurkan <selam@gurkanuzunca.com>
2016-03-27 20:25:01 +0300

    Template

--- templates/index.php
+++ templates/index.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MoneyLira</title>
 
-
     <link rel="stylesheet" href="assets/libs/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/libs/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="assets/css/main.css">
@@ -24,9 +23,22 @@
 
 
 <header id="header">
-    <div class="container">
-        <div class="logo">
-            Money Lira
+    <div class="head">
+        <div class="container">
+            <div class="logo">
+                MONEYLİRA
+            </div>
+        </div>
+    </div>
+
+    <div class="navigation">
+        <div class="container">
+            <ul>
+                <li><a href="#">Dashboard</a></li>
+                <li><a href="#">Kişiler</a></li>
+                <li><a href="#">Borçlar</a></li>
+                <li><a href="#">Alacaklar</a></li>
+            </ul>
         </div>
     </div>
 
@@ -35,13 +47,51 @@
 
 <main id="main">
     <div class="container">
-        <h1 class="page-title">Blank Page</h1>
+        <h1 class="page-header">Blank Page</h1>
 
         <ol class="breadcrumb">
             <li><a href="#">Anasayfa</a></li>
             <li><a href="#">Pages</a></li>
             <li class="active">Blank Page</li>
         </ol>
+
+        <div class="row">
+            <div class="col-sm-8">
+                <div class="card panel panel-default">
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-sm-4">
+
+                                <div class="person-name">
+                                    <div class="person-shortname">HB</div>
+                                    <div class="person-fullname">Hasan Baldan</div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-4">
+                                <div class="date">
+                                    12-10-2016
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="panel panel-default">
+
+                    <div class="panel-body">
+                        <h3 class="panel-caption">EYLEMLER</h3>
+                        <p class="panel-caption-text">Son eylemler listesi</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
     </div>
 </main>
 
